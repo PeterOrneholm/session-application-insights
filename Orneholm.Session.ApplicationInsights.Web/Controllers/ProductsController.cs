8eeb8101d57edc5bd70e97ca4219d329ca9fc6c5
Peter Örneholm <peter@orneholm.com>
2014-09-28 13:59:43 +0200

    First version

---
+++ Orneholm.Session.ApplicationInsights.Web/Controllers/ProductsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Web.Http;
+
+namespace Orneholm.Session.ApplicationInsights.Web.Controllers
+{
+    [RoutePrefix("api")]
+    public class ProductsController : ApiController
+    {
+        private readonly List<Product> _products;
+
+        public ProductsController()
+        {
+            _products = new List<Product>()
+            {
+                new Product() {id = 1, category = "Tech", name = "Computer"},
+                new Product() {id = 2, category = "Tech", name = "Phone"},
+                new Product() {id = 3, category = "Tech", name = "TV"},
+
+                new Product() {id = 4, category = "Food", name = "Potatoes"},
+                new Product() {id = 5, category = "Food", name = "Hamburger"},
+                new Product() {id = 6, category = "Food", name = "Pizza"},
+
+                new Product() {id = 7, category = "Sport", name = "Basketball"},
+                new Product() {id = 8, category = "Sport", name = "Football"},
+                new Product() {id = 9, category = "Sport", name = "Baseball"}
+            };
+        }
+
+        [Route("products")]
+        public IEnumerable<Product> GetProducts()
+        {
+            return _products;
+        }
+
+        [Route("products/{category}")]
+        public IEnumerable<Product> GetProducts(string category)
+        {
+            if (category.Equals("food", StringComparison.InvariantCultureIgnoreCase))
+            {
+                Thread.Sleep(3000);
+            }
+
+            return _products.Where(x => x.category.Equals(category, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public class Product
+        {
+            public int id { get; set; }
+            public string name { get; set; }
+            public string category { get; set; }
+        }
+    }
+}
---
+++ Orneholm.Session.ApplicationInsights.Web/Global.asax.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Http;
+
+namespace Orneholm.Session.ApplicationInsights.Web
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+        }
+    }
+}
